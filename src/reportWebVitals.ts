65d754bf2e35a657b78223cd90e52f0235ccb186
jessethouin <github-jt23@sneakemail.com>
2024-12-09 18:20:55 -0500

    Initial commit.

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,15 @@
+import { MetricType } from "web-vitals";
+
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
+  if (onPerfEntry) {
+    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
